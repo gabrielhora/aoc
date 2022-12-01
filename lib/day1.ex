ecd3e5cf1015507ec25b3cd983e54bc0a6f351bf
Gabriel Hora <gabrielhora@gmail.com>
2022-12-01 15:35:35 +0100

    year 2022, day 1

---
+++ lib/day1.ex
@@ -0,0 +1,21 @@
+defmodule Day1 do
+  @spec part1([String.t()]) :: integer
+  def part1(lines) do
+    lines
+    |> Enum.chunk_while([], &chunk_on_space/2, &{:cont, &1})
+    |> Enum.reduce([], &[Enum.sum(&1) | &2])
+    |> Enum.max()
+  end
+
+  def part2(lines) do
+    lines
+    |> Enum.chunk_while([], &chunk_on_space/2, &{:cont, &1})
+    |> Enum.reduce([], &[Enum.sum(&1) | &2])
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  defp chunk_on_space("", acc), do: {:cont, acc, []}
+  defp chunk_on_space(el, acc), do: {:cont, [String.to_integer(el) | acc]}
+end
