f7bf811904e40b615eb94c93e463c62d6ef28597
Gabriel Hora <gabrielhora@gmail.com>
2022-12-02 09:38:06 +0100

    less repetition

--- lib/day2.ex
+++ lib/day2.ex
@@ -2,36 +2,40 @@ defmodule Day2 do
   def part1(lines) do
     lines
     |> Enum.map(&String.split/1)
-    |> Enum.reject(&(&1 == []))
-    |> Enum.map(&score_part1/1)
+    |> Enum.map(
+      &case &1 do
+        ["A", "X"] -> 1 + 3
+        ["A", "Y"] -> 2 + 6
+        ["A", "Z"] -> 3 + 0
+        ["B", "X"] -> 1 + 0
+        ["B", "Y"] -> 2 + 3
+        ["B", "Z"] -> 3 + 6
+        ["C", "X"] -> 1 + 6
+        ["C", "Y"] -> 2 + 0
+        ["C", "Z"] -> 3 + 3
+        _ -> 0
+      end
+    )
     |> Enum.sum()
   end
-
-  defp score_part1(["A", "X"]), do: 1 + 3
-  defp score_part1(["A", "Y"]), do: 2 + 6
-  defp score_part1(["A", "Z"]), do: 3 + 0
-  defp score_part1(["B", "X"]), do: 1 + 0
-  defp score_part1(["B", "Y"]), do: 2 + 3
-  defp score_part1(["B", "Z"]), do: 3 + 6
-  defp score_part1(["C", "X"]), do: 1 + 6
-  defp score_part1(["C", "Y"]), do: 2 + 0
-  defp score_part1(["C", "Z"]), do: 3 + 3
 
   def part2(lines) do
     lines
     |> Enum.map(&String.split/1)
-    |> Enum.reject(&(&1 == []))
-    |> Enum.map(&score_part2/1)
+    |> Enum.map(
+      &case &1 do
+        ["A", "X"] -> 3 + 0
+        ["A", "Y"] -> 1 + 3
+        ["A", "Z"] -> 2 + 6
+        ["B", "X"] -> 1 + 0
+        ["B", "Y"] -> 2 + 3
+        ["B", "Z"] -> 3 + 6
+        ["C", "X"] -> 2 + 0
+        ["C", "Y"] -> 3 + 3
+        ["C", "Z"] -> 1 + 6
+        _ -> 0
+      end
+    )
     |> Enum.sum()
   end
-
-  defp score_part2(["A", "X"]), do: 3 + 0
-  defp score_part2(["A", "Y"]), do: 1 + 3
-  defp score_part2(["A", "Z"]), do: 2 + 6
-  defp score_part2(["B", "X"]), do: 1 + 0
-  defp score_part2(["B", "Y"]), do: 2 + 3
-  defp score_part2(["B", "Z"]), do: 3 + 6
-  defp score_part2(["C", "X"]), do: 2 + 0
-  defp score_part2(["C", "Y"]), do: 3 + 3
-  defp score_part2(["C", "Z"]), do: 1 + 6
 end
