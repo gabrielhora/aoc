3dbe97bff58cbc7414a92cac5579a2ec7cc81457
Gabriel Hora <gabrielhora@gmail.com>
2022-12-03 18:17:55 +0100

    easier to read

--- lib/day3.ex
+++ lib/day3.ex
@@ -2,9 +2,11 @@ defmodule Day3 do
   def part1(lines) do
     lines
     |> Enum.map(&String.split_at(&1, div(String.length(&1), 2)))
-    |> Enum.map(&{String.graphemes(elem(&1, 0)), String.graphemes(elem(&1, 1))})
-    |> Enum.map(&[MapSet.new(elem(&1, 0)), MapSet.new(elem(&1, 1))])
-    |> Enum.map(&MapSet.intersection(Enum.at(&1, 0), Enum.at(&1, 1)))
+    |> Enum.map(fn el ->
+      fst = MapSet.new(String.graphemes(elem(el, 0)))
+      snd = MapSet.new(String.graphemes(elem(el, 1)))
+      MapSet.intersection(fst, snd) |> Enum.at(0)
+    end)
     |> Enum.map(&score/1)
     |> Enum.sum()
   end
@@ -13,13 +15,13 @@ defmodule Day3 do
     lines
     |> Enum.chunk_every(3)
     |> Enum.map(fn el -> Enum.map(el, &MapSet.new(String.graphemes(&1))) end)
-    |> Enum.map(fn el -> Enum.reduce(el, &MapSet.intersection(&1, &2)) end)
+    |> Enum.map(fn el -> Enum.reduce(el, &MapSet.intersection(&1, &2)) |> Enum.at(0) end)
     |> Enum.map(&score/1)
     |> Enum.sum()
   end
 
-  defp score(map) do
-    letter = Enum.at(map, 0) |> :binary.first()
-    if letter in ?a..?z, do: letter - 96, else: letter - 64 + 26
+  defp score(letter) do
+    asc = :binary.first(letter)
+    if asc in ?a..?z, do: asc - 96, else: asc - 64 + 26
   end
 end
