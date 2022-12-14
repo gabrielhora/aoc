1e1f1159dbed7948888b68ebd9857ec98c89408e
Gabriel Hora <gabrielhora@gmail.com>
2022-12-14 10:48:25 +0100

    reduce legibility 😆

--- lib/Y2022/day13.ex
+++ lib/Y2022/day13.ex
@@ -4,7 +4,7 @@ defmodule Y2022.Day13 do
     |> String.split("\n\n")
     |> Enum.map(&parse_packet/1)
     |> Enum.with_index(1)
-    |> Enum.filter(fn {[l, r], _} -> compare(l, r) end)
+    |> Enum.filter(fn {[l, r], _} -> cmp(l, r) end)
     |> Enum.map(&elem(&1, 1))
     |> Enum.sum()
   end
@@ -14,7 +14,7 @@ defmodule Y2022.Day13 do
     |> String.split("\n\n")
     |> Enum.flat_map(&parse_packet/1)
     |> then(&(&1 ++ [[[2]], [[6]]]))
-    |> Enum.sort(&compare/2)
+    |> Enum.sort(&cmp/2)
     |> Enum.with_index(1)
     |> Enum.filter(fn {p, _} -> p == [[2]] || p == [[6]] end)
     |> Enum.map(&elem(&1, 1))
@@ -23,26 +23,19 @@ defmodule Y2022.Day13 do
 
   # compare returns true if the lists are in the correct order
 
-  defp compare([left | lt], [right | rt]) when is_integer(left) and is_integer(right) do
-    cond do
-      left < right -> true
-      left > right -> false
-      true -> compare(lt, rt)
+  def cmp(left, right) do
+    case {left, right} do
+      {[], r} when is_list(r) -> true
+      {l, []} when is_list(l) -> false
+      {[l | _], [r | _]} when l < r and is_integer(l) and is_integer(r) -> true
+      {[l | _], [r | _]} when l > r and is_integer(l) and is_integer(r) -> false
+      {[l | ls], [r | rs]} when is_integer(l) and is_integer(r) -> cmp(ls, rs)
+      {[l | ls], r} when is_integer(l) and is_list(r) -> cmp([[l] | ls], r)
+      {l, [r | rs]} when is_integer(r) and is_list(l) -> cmp(l, [[r] | rs])
+      {[l | ls], [r | rs]} when l == r and is_list(l) and is_list(r) -> cmp(ls, rs)
+      {[l | _], [r | _]} when is_list(l) and is_list(r) -> cmp(l, r)
     end
   end
-
-  defp compare([], right) when is_list(right), do: true
-  defp compare(left, []) when is_list(left), do: false
-
-  defp compare([left | lt], [right | rt]) when is_list(left) and is_list(right) do
-    cond do
-      left == right -> compare(lt, rt)
-      true -> compare(left, right)
-    end
-  end
-
-  defp compare([left | t], right) when is_integer(left), do: compare([[left] | t], right)
-  defp compare(left, [right | t]) when is_integer(right), do: compare(left, [[right] | t])
 
   defp parse_packet(line) do
     String.split(line, "\n")
