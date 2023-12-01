12ec6ceb2bd92d7a675f2700428c3743359a253a
Gabriel Hora <gabrielhora@gmail.com>
2023-12-01 09:59:16 +0000

    day 1 year 2023 improvements

--- go/2023/day1/main.go
+++ go/2023/day1/main.go
@@ -20,8 +20,8 @@ func part1(input string) {
 	data := parse(input)
 	sum := 0
 	for _, line := range data {
-		ns := findFirstNum(line, false) + findLastNum(line, false)
-		n, _ := strconv.Atoi(ns)
+		nums := findNumbers(line, false)
+		n, _ := strconv.Atoi(nums[0] + nums[len(nums)-1])
 		sum += n
 	}
 	fmt.Printf("part 1: %v\n", sum)
@@ -31,50 +31,35 @@ func part2(input string) {
 	data := parse(input)
 	sum := 0
 	for _, line := range data {
-		ns := findFirstNum(line, true) + findLastNum(line, true)
-		n, _ := strconv.Atoi(ns)
+		nums := findNumbers(line, true)
+		n, _ := strconv.Atoi(nums[0] + nums[len(nums)-1])
 		sum += n
 	}
 	fmt.Printf("part 2: %v\n", sum)
 }
 
-func findFirstNum(line []rune, findWords bool) string {
+func findNumbers(line []rune, findWords bool) []string {
 	acc := ""
+	var nums []string
 	for i := 0; i < len(line); i++ {
 		if findWords {
 			acc += string(line[i])
 			if val := hasNumberWord(acc); val != "" {
-				return val
+				nums = append(nums, val)
 			}
 		}
 		if unicode.IsDigit(line[i]) {
-			return string(line[i])
+			nums = append(nums, string(line[i]))
 		}
 	}
-	panic("should have found first number")
-}
-
-func findLastNum(line []rune, findWords bool) string {
-	acc := ""
-	for i := len(line) - 1; i >= 0; i-- {
-		if findWords {
-			acc = string(line[i]) + acc // prepend
-			if val := hasNumberWord(acc); val != "" {
-				return val
-			}
-		}
-		if unicode.IsDigit(line[i]) {
-			return string(line[i])
-		}
-	}
-	panic("should have found last number")
+	return nums
 }
 
 var numWords = map[string]string{"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 
 func hasNumberWord(val string) string {
 	for name, num := range numWords {
-		if strings.Contains(val, name) {
+		if strings.HasSuffix(val, name) {
 			return num
 		}
 	}
