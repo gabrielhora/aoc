a13e6c9a6af292fc1cb219bf19fcbdbcd2cfdfb1
Gabriel Hora <gabrielhora@gmail.com>
2023-12-11 16:12:38 +0000

    day 11 year 2023 no need to walk the shortest path, just need the distance

--- go/2023/day11/main.go
+++ go/2023/day11/main.go
@@ -33,16 +33,20 @@ func sumDistances(image [][]string, galaxies []point, weight int) int {
 	weight = max(1, weight-1)
 	totalDistance := 0
 	p := pairs(galaxies)
-	for i, pair := range p {
-		if i%1000 == 0 {
-			fmt.Printf("%d / %d\n", i, len(p))
-		}
+	for _, pair := range p {
 		nr := newRowsBetween(image, pair[0].y, pair[1].y)
 		nc := newColsBetween(image, pair[0].x, pair[1].x)
-		dist := shortestPath(image, pair[0], pair[1])
+		dist := abs(pair[0].y-pair[1].y) + abs(pair[0].x-pair[1].x)
 		totalDistance += dist + nr*weight + nc*weight
 	}
 	return totalDistance
+}
+
+func abs(val int) int {
+	if val < 0 {
+		return -val
+	}
+	return val
 }
 
 func newRowsBetween(image [][]string, p1, p2 int) int {
@@ -96,52 +100,6 @@ func pairs(points []point) [][2]point {
 		}
 	}
 	return result
-}
-
-type queueItem struct {
-	point
-	distance int
-}
-
-func shortestPath(matrix [][]string, start, end point) int {
-	queue := []queueItem{{start, 0}}
-	visited := map[point]struct{}{}
-	visited[start] = struct{}{}
-
-	for len(queue) > 0 {
-		// pop first
-		item := queue[0]
-		queue = queue[1:]
-
-		// reached end?
-		if item.point == end {
-			smallest := item.distance
-			for _, q := range queue {
-				if q.distance < smallest {
-					smallest = q.distance
-				}
-			}
-			return smallest
-		}
-
-		// visit neighbours
-		neighbours := []point{
-			{item.x - 1, item.y},
-			{item.x + 1, item.y},
-			{item.x, item.y - 1},
-			{item.x, item.y + 1},
-		}
-		for _, n := range neighbours {
-			inBounds := n.x >= 0 && n.x < len(matrix[0]) && n.y >= 0 && n.y < len(matrix)
-			_, isVisited := visited[n]
-			if inBounds && !isVisited {
-				queue = append(queue, queueItem{n, item.distance + 1})
-				visited[n] = struct{}{}
-			}
-		}
-	}
-
-	return 0
 }
 
 func parse(input string) [][]string {
