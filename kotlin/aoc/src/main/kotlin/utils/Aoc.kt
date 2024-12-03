168a8eb1e10e88bc62611cebd503d0a2b4e96f8b
Gabriel Hora <gabrielhora@gmail.com>
2024-12-03 14:34:21 +0000

    clean up 2024 day 3

--- kotlin/aoc/src/main/kotlin/utils/Aoc.kt
+++ kotlin/aoc/src/main/kotlin/utils/Aoc.kt
@@ -3,3 +3,5 @@ package utils
 fun readFile(path: String) = object {}.javaClass.getResource(path)?.readText() ?: throw Exception("no file $path")
 
 fun List<String>.listOfInts() = this.map { line -> line.split("\\s+".toRegex()).map { it.toInt() } }
+
+fun String.findAll(pattern: String) = Regex(pattern).findAll(this)
--- kotlin/aoc/src/main/kotlin/y2024/Day3.kt
+++ kotlin/aoc/src/main/kotlin/y2024/Day3.kt
@@ -1,48 +1,28 @@
 package y2024
 
-sealed interface Op
-data class Mul(val first: Int, val second: Int) : Op
-data object Do : Op
-data object Dont : Op
+import utils.findAll
 
 class Day3 {
     fun part1(input: String): Int {
-        return parsePart1(input.replace("\n", ""))
-            .sumOf { it.first * it.second }
+        return input
+            .findAll("mul\\((\\d+),(\\d+)\\)")
+            .map { it.groupValues.drop(1) }
+            .sumOf { (fst, snd) -> fst.toInt() * snd.toInt() }
     }
 
     fun part2(input: String): Int {
-        return parsePart2(input.replace("\n", ""))
-            .fold(0 to true) { acc, op ->
+        return input
+            .findAll("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)")
+            .fold(0 to 1) { acc, match ->
+                val gv = match.groupValues
+                val op = gv.first()
                 when {
-                    op is Do -> acc.first to true
-                    op is Dont -> acc.first to false
-                    acc.second && op is Mul -> (acc.first + op.first * op.second) to true
-                    else -> acc
+                    op == "do()" -> acc.first to 1
+                    op == "don't()" -> acc.first to 0
+                    op.startsWith("mul") -> acc.first + acc.second * gv[1].toInt() * gv[2].toInt() to acc.second
+                    else -> throw Exception("invalid")
                 }
             }
             .first
     }
-
-    private fun parsePart1(input: String): Sequence<Pair<Int, Int>> {
-        val matches = Regex("mul\\((\\d+),(\\d+)\\)").findAll(input)
-        return matches
-            .map { it.groupValues.drop(1) }
-            .map { (fst, snd) -> fst.toInt() to snd.toInt() }
-    }
-
-    private fun parsePart2(input: String): Sequence<Op> {
-        val matches = Regex("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)").findAll(input)
-        return matches.map {
-            val values = it.groupValues
-            val op = values.first()
-            when {
-                op.startsWith("mul") -> Mul(values[1].toInt(), values[2].toInt())
-                op == "do()" -> Do
-                op == "don't()" -> Dont
-                else -> throw Exception("unknown value")
-            }
-        }
-    }
-
 }
