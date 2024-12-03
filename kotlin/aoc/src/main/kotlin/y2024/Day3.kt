f9b2990973146945fae708b7f2259cd30319aa11
Gabriel Hora <gabrielhora@gmail.com>
2024-12-03 14:34:21 +0000

    clean up 2024 day 3

--- kotlin/aoc/src/main/kotlin/y2024/Day3.kt
+++ kotlin/aoc/src/main/kotlin/y2024/Day3.kt
@@ -1,48 +1,44 @@
 package y2024
 
+import utils.findAll
+
 sealed interface Op
-data class Mul(val first: Int, val second: Int) : Op
+data class Mul(val fst: Int, val snd: Int) : Op
 data object Do : Op
 data object Dont : Op
 
 class Day3 {
-    fun part1(input: String): Int {
-        return parsePart1(input.replace("\n", ""))
-            .sumOf { it.first * it.second }
-    }
+    fun part1(input: String): Int = input.parsePart1().sumOf { it.fst * it.snd }
 
-    fun part2(input: String): Int {
-        return parsePart2(input.replace("\n", ""))
+    fun part2(input: String): Int =
+        input
+            .parsePart2()
             .fold(0 to true) { acc, op ->
                 when {
                     op is Do -> acc.first to true
                     op is Dont -> acc.first to false
-                    acc.second && op is Mul -> (acc.first + op.first * op.second) to true
+                    acc.second && op is Mul -> (acc.first + op.fst * op.snd) to true
                     else -> acc
                 }
             }
             .first
-    }
 
-    private fun parsePart1(input: String): Sequence<Pair<Int, Int>> {
-        val matches = Regex("mul\\((\\d+),(\\d+)\\)").findAll(input)
-        return matches
+    private fun String.parsePart1(): Sequence<Mul> =
+        findAll("mul\\((\\d+),(\\d+)\\)")
             .map { it.groupValues.drop(1) }
-            .map { (fst, snd) -> fst.toInt() to snd.toInt() }
-    }
+            .map { (fst, snd) -> Mul(fst.toInt(), snd.toInt()) }
 
-    private fun parsePart2(input: String): Sequence<Op> {
-        val matches = Regex("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)").findAll(input)
-        return matches.map {
-            val values = it.groupValues
-            val op = values.first()
-            when {
-                op.startsWith("mul") -> Mul(values[1].toInt(), values[2].toInt())
-                op == "do()" -> Do
-                op == "don't()" -> Dont
-                else -> throw Exception("unknown value")
+    private fun String.parsePart2(): Sequence<Op> =
+        findAll("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)")
+            .map {
+                val values = it.groupValues
+                val op = values.first()
+                when {
+                    op.startsWith("mul") -> Mul(values[1].toInt(), values[2].toInt())
+                    op == "do()" -> Do
+                    op == "don't()" -> Dont
+                    else -> throw Exception("unknown value")
+                }
             }
-        }
-    }
 
 }
